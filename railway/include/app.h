1e92509bb4e77b7ec4650357c0e16bd18ae36376
maisvendoo <maisvendoo@yandex.ru>
2022-08-17 00:50:25 +0300

    Создан каркас приложения для модуля моделирования работы участка дороги

---
+++ railway/include/app.h
@@ -0,0 +1,27 @@
+#ifndef     APP_H
+#define     APP_H
+
+#include    <QCoreApplication>
+#include    <QCommandLineParser>
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+class App : public QCoreApplication
+{
+    Q_OBJECT
+
+public:
+
+    explicit App(int argc, char *argv[]);
+
+    ~App();
+
+    int exec();
+
+private:
+
+
+};
+
+#endif // APP_H
